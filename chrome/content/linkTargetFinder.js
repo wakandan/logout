a64942859aafccdcef3b128e45a3f488e30f56a4
Dang Nguyen Anh Khoa <wakandan@gmail.com>
2011-04-03 18:36:07 +0800

    Still one page logout, but introduced a event-type logout method.
    Create a mouse click event, find the link or logout element, then dispatch the event on the element. Work brilliantly. :)

--- chrome/content/linkTargetFinder.js
+++ chrome/content/linkTargetFinder.js
@@ -15,6 +15,8 @@ var linkTargetFinder = function () {
 			//var link = content.document.getElementById("gb_71");
 			//content.document.location = link.attributes['href'].value;
             var expressions = new Array();			
+
+            //put all the sign out signature into an array
 			var keywords = new Array();
 			keywords.push("Logout");
 			keywords.push("Log out");
@@ -22,8 +24,27 @@ var linkTargetFinder = function () {
 			keywords.push("Signout");
 			keywords.push("Log Off");
 
+            //var logout_maping = {
+            //    "sinprd0103.outlook.com": "/owa/logoff.owa"
+            //};
 
-			//find all the links first
+            //Firebug.Console.log(logout_maping);
+            //
+            //var cur_host = content.location.hostname;
+            //
+
+            //try{
+            //    var link = logout_maping[cur_host];
+            //    var log_out_link = content.location.protocol + "//" + cur_host + link;
+            //    alert(log_out_link);
+            //    content.document.location = log_out_link;
+            //}catch(e){
+            //    Firebug.Console.log(e);
+            //}
+            
+
+			//find all the links that looks like a sign out
+            //constructing an xPath expression to recognize LINKS
 			var xPath_expression = "";
 			for(var i = 0; i<keywords.length; i++){
 				if(i==0){
@@ -35,8 +56,7 @@ var linkTargetFinder = function () {
 			var exp = "//a/text()["+xPath_expression+"]";
             expressions.push(exp);
 
-
-			//find all element that can be used to sign out
+            //constructing an xPath expression to recognize others, like FORM 
 			var xPath_expression = "";
 			for(var i = 0; i<keywords.length; i++){
 				if(i==0){
@@ -49,32 +69,46 @@ var linkTargetFinder = function () {
             expressions.push(exp);
            
             var expression = expressions.join(" | ");
-            Firebug.Console.log(expression);
-
 			var nodes = content.document.evaluate(expression, content.document, null, XPathResult.ANY_TYPE, null);
 
+
+            //Create a button click event, copy from stack overflow. 
+            //This method seems to work only on firefox.
+            //example: http://www.howtocreate.co.uk/tutorials/javascript/domevents
+            var event = content.document.createEvent("MouseEvents");
+            event.initEvent("click", true, true, content.window, 0, 0, 0, 0, 0,  false, false, false, false,0, null);
+
+            //loop over all the objects
 			var x = nodes.iterateNext();
 			while(x!=null){			
-				//first try
-                try{                    
-    				content.document.location = x.parentNode.attributes['href'].value;
-                } catch(e){
-                    Firebug.Console.log(e);
+
+                try{
+                    //try to manually click the link first. This should work most of the time
+                    x.dispatchEvent(event);
+                }catch(e){
+                    Firebug.Console.log("Manually click failed", e);
                 }
-				
-				//second try
+
+				//something that might clickable
                 try{
                     x.click();
                 }catch(e){
-                    Firebug.Console.log(e);
+                    Firebug.Console.log("Second try failed", e);
                 }
 
-                //third try
+                //third try. In case it's form like in facebook
                 try{
                     x.parentNode.click();
                 }catch(e){
-                    Firebug.Console.log(e);
+                    Firebug.Console.log("Third try failed", e);
                 }
+				//first try
+                //try{                                       
+                //    var tmp_href = x.parentNode.attributes['href'].value; 
+    			//	content.document.location = tmp_href;
+                //} catch(e){
+                //    Firebug.Console.log(e);
+                //}
 				x = nodes.iterateNext();
 			}
 		}
